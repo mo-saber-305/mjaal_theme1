564e81c403ca5e42502db9e4a1ec2359fb01620d
Mo Saber <54186055+mo-saber-305@users.noreply.github.com>
2024-12-10 20:18:03 +0200

    finish on home page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,53 @@
 $(document).ready(function () {
-    $('#home_page .partners .partners-box').owlCarousel({
+    $('header .hero .hero-box').owlCarousel({
         loop: false,
+        rtl: true,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+            },
+
+        }
+    });
+
+    let features_slider_owl = $('#home_page .features .slider-box .right-box .slider');
+    features_slider_owl.owlCarousel({
+        loop: true,
+        rtl: true,
+        responsiveClass: true,
+        autoplay: true,
+        autoplayTimeout: 5000,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1,
+            },
+        }
+    });
+
+    features_slider_owl.on('changed.owl.carousel', function (event) {
+        const currentIndex = event.item.index - event.relatedTarget._clones.length / 2;
+        const totalItems = event.item.count;
+        const normalizedIndex = (currentIndex + totalItems) % totalItems;
+        $('#home_page .features .slider-box .left-box .card .card-body .item').removeClass('active');
+        $(`#home_page .features .slider-box .left-box .card .card-body .item[data-index="${normalizedIndex}"]`).addClass('active');
+    });
+
+    $('#home_page .features .slider-box .left-box .card .card-body .item').on('click', function () {
+        const index = $(this).data('index');
+        $(this).addClass('active').siblings().removeClass('active')
+        features_slider_owl.trigger('to.owl.carousel', [index, 300]);
+    });
+
+    $('#home_page .partners .partners-box').owlCarousel({
+        loop: true,
         margin: 30,
         rtl: true,
         responsiveClass: true,
+        autoplay: true,
+        autoplayTimeout: 5000,
+        autoplayHoverPause: true,
         responsive: {
             0: {
                 items: 1,
@@ -22,6 +66,20 @@ $(document).ready(function () {
             }
         }
     });
+
+    $('#home_page .website-control .website-control-box').owlCarousel({
+        loop: true,
+        rtl: true,
+        responsiveClass: true,
+        autoplay: true,
+        autoplayTimeout: 5000,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1,
+            },
+        }
+    })
 
     $('#home_page .testimonials .testimonials-box').owlCarousel({
         // stagePadding: 60,
@@ -48,4 +106,35 @@ $(document).ready(function () {
             },
         }
     })
+
+    let donations_slider_owl = $('#home_page .donations .right-box .donations-box')
+
+    donations_slider_owl.owlCarousel({
+        loop: true,
+        rtl: true,
+        dots: false,
+        responsiveClass: true,
+        autoplay: true,
+        autoplayTimeout: 5000,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1,
+            },
+        }
+    })
+
+    donations_slider_owl.on('changed.owl.carousel', function (event) {
+        const currentIndex = event.item.index - event.relatedTarget._clones.length / 2;
+        const totalItems = event.item.count;
+        const normalizedIndex = (currentIndex + totalItems) % totalItems;
+        $('#home_page .donations .left-box .item').removeClass('active');
+        $(`#home_page .donations .left-box .item[data-index="${normalizedIndex}"]`).addClass('active');
+    });
+
+    $('#home_page .donations .left-box .item').on('click', function () {
+        const index = $(this).data('index');
+        $(this).addClass('active').closest('.item-box').siblings().find('.item').removeClass('active')
+        donations_slider_owl.trigger('to.owl.carousel', [index, 300]);
+    });
 });
